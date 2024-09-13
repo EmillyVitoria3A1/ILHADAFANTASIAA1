bc5dad39e112ddc0f4f77d03ed8a65d7907045b0
EmillyVitoria3A1 <emilly.vitoria250806@gmail.com>
2024-09-13 12:15:58 -0300

    hggj

---
+++ script.js
@@ -0,0 +1,7 @@
+const avanca = document.guerySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+    })
+})
